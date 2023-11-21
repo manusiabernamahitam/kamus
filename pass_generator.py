088f424b7b28f1bb6c9800516fce40434a694343
black <151435067+manusiabernamahitam@users.noreply.github.com>
2023-11-21 20:18:33 +0700

    Add files via upload
    strong password

---
+++ pass_generator.py
@@ -0,0 +1,9 @@
+import random
+
+sandi = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+panjang = int(input("berapa panjang karakter untuk kata sandi"))
+hasil = ""
+
+for i in range(panjang):
+    hasil += random.choice(sandi)
+print("kata sandi yang di hasilkan adalah",hasil)
